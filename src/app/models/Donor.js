20657c1ca0403a9129b5a5cc6aaa2ec58e5e75e0
Bruno-Goudric <brunolfsports@hotmail.com>
2020-02-26 15:04:41 -0300

    Create a Page Doe_Sangue

---
+++ src/app/models/Donor.js
@@ -0,0 +1,38 @@
+const db = require('../../config/db')
+const {  date } = require('../../lib/utils')
+
+module.exports = {
+    all(callback){
+        db.query(`
+        SELECT * FROM donor`, function(err, results){
+            if(err) throw "Database Error!"
+
+            callback(results.rows)
+            
+
+        })
+    },
+    create(data, callback){
+        const query = `
+            INSERT INTO donor (
+                name,
+                email,
+                blood,
+                created_at
+            ) VALUES ($1,$2,$3,$4)
+            RETURNING id
+        `
+        const values = [
+            data.name,
+            data.email,
+            data.blood,
+            date(Date.now()).iso
+        ]
+        db.query(query, values, function(err, results) {
+            if(err) throw `Database Error! ${err}`
+
+
+            callback(results.rows)
+        })
+    }
+}
